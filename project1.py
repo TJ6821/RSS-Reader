e00a507f613ef1e9b7901dc63d65e00f9bccbd9e
Tony <twalt026@gmail.com>
2019-03-03 19:22:41 -0500

    Fixed some coding mistakes that were 2.7 specific in a 3.0+ environment.

--- project1.py
+++ project1.py
@@ -4,7 +4,5 @@ import feedparser
 
 myRss = feedparser.parse("https://nyaa.si/?page=rss")
 
-print 'Number of RSS posts :', len(myRss.entries)
-myList = myRSS.myList[1]
+print (myRss['feed']['title'])
 
-print 'Post Title : ',myList.title
